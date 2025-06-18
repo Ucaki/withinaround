9b8e11768ee157dbe9c256b4658ee2af42cd871f
Ucaki <uros.andjelkovic1997@gmail.com>
2025-06-18 12:35:37 +0200

    fix: play with style, ignore this commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <nav className="bg-yellow-900 text-white p-4 flex gap-19">
+    <nav className="bg-yellow-900 text-white p-1 flex gap-19">
      <div style={{ width : '16%', height: '1%'}}>
                <Image
                  src="/withinaroundLogo.png"
@@ -13,7 +13,7 @@ export default function Navbar() {
                  height={150}
                />
       </div>
-     <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center',  width: '18%'}}>
+     <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '18%'}}>
         <Link href="/">Home/About</Link>
       {/* <Link href="/about">About</Link> */}
         <Link href="/workshops">Workshops</Link>
